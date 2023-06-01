0132cf9440a12d85a1a97656010602c5c37929f1
manvernegi <manvernegi00@gmail.com>
2023-06-01 14:01:51 +0530

    first commit

---
+++ customer/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from customer.models import *
+
+class GetCustomerserializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Customers
+        fields = "__all__"
+
+class GetAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerAddress
+        fields = "__all__"
+
+class CustomerDetailsAddressSerializer(serializers.ModelSerializer):
+    customer_address = GetAddressSerializer(many = True)
+    
+    class Meta:
+        model = Customers
+        fields = ('first_name','last_name','mobile','address','age','country','city')
---
+++ customer/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path,include
+from customer.views import *
+urlpatterns = [
+    path ('get-customers',GetCustomerView.as_view()),    
+    path ('get-Address',GetAddressView.as_view()),
+]      
